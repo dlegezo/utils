7472063c4bae9b923d79086f75a2e87bf165f286
Denis Legezo <Denis.Legezo@kaspersky.com>
2018-03-26 10:31:34 +0300

    regular commit

--- c/shikata_decrypt.c
+++ c/shikata_decrypt.c
@@ -5,18 +5,19 @@
 #include <string.h>
 
 #define shikata_decryptor 0xDA, 0xD4, 0xD9, 0x74, 0x24, 0xF4, 0x5B, 0xBF
-#define shikata_initial_key_offset 0x08
-#define shikata_decryption_len_offset 0x10
-#define shikata_encrypted_block_offset 0x1A
 
-struct shikata_header_struct {
+struct shikata_prolog_struct {
 	uint8_t first_code[8];
 	uint8_t initial_key[4];
 	uint8_t second_code[4];
 	uint8_t decryption_len[2];
 	uint8_t third_code[8];
-	uint8_t *encrypted_block;
-} shikata_header;
+	uint8_t encrypted_block[];
+};
+
+shikata_prolog_struct *transform_to_prolog(uint8_t *data) {
+	return((shikata_prolog_struct *)data);
+}
 
 uint8_t *knuth_morris_pratt_failure(uint8_t *pattern, int pattern_length) {
 	uint8_t *failure = (uint8_t *)malloc(pattern_length);
@@ -55,6 +56,23 @@ int knuth_morris_pratt_search(uint8_t *data, uint8_t *pattern, int data_length, 
 	return(-1);
 }
 
+int get_int_value(uint8_t *data, int length) {
+	int result = 0,
+		i = length;
+	while (i) {
+		result <<= 8;
+		result |= data[--i];
+	}
+	return(result);
+}
+
+int set_int_value(uint8_t *buffer, int value) {
+	*buffer = value & 0xFF;
+	buffer[1] = (value >> 8) & 0xFF;
+	buffer[2] = (value >> 16) & 0xFF;
+	buffer[3] = (value >> 24) & 0xFF;
+}
+
 int get_file_size(FILE *hEncoded) {
 	int file_size = -1;
 	fseek(hEncoded, 0L, SEEK_END);
@@ -70,38 +88,15 @@ int find_shikata(uint8_t *shikata_file_code, int data_length) {
 	return(shikata_offset);
 }
 
-int get_shikata_initial_key(uint8_t *shikata_file_code, int shikata_offset) {
-	int initial_key;
-	initial_key = shikata_file_code[shikata_offset + shikata_initial_key_offset + 3];
-	initial_key <<= 8;
-	initial_key |= shikata_file_code[shikata_offset + shikata_initial_key_offset + 2];
-	initial_key <<= 8;
-	initial_key |= shikata_file_code[shikata_offset + shikata_initial_key_offset + 1];
-	initial_key <<= 8;
-	initial_key |= shikata_file_code[shikata_offset + shikata_initial_key_offset];
-	return(initial_key);
-}
-
-int get_shikata_decryption_len(uint8_t *shikata_file_code, int shikata_offset) {
-	int decryption_len;
-	decryption_len = shikata_file_code[shikata_decryption_len_offset + 1];
-	decryption_len <<= 8;
-	decryption_len |= shikata_file_code[shikata_decryption_len_offset];
-	return(decryption_len);
-}
-
-uint8_t decrypt_shikata(uint8_t *shikata_file_code, int shikata_offset, int shikata_initial_key, int decryption_len) {
-	int i = 0;
-	int xor_key = shikata_initial_key,
-		new_int_value;
+uint8_t decrypt_shikata_new(struct shikata_prolog_struct *code) {
+	int xor_key = get_int_value(code->initial_key, 4),
+		decryption_len = get_int_value(code->decryption_len, 2),
+		new_int_value,
+		i = 0;
 	while (i < decryption_len) {
-		shikata_file_code[shikata_offset + shikata_encrypted_block_offset + i*4] ^= xor_key;
-		shikata_file_code[shikata_offset + shikata_encrypted_block_offset + i*4 + 1] ^= (xor_key >> 8);
-		shikata_file_code[shikata_offset + shikata_encrypted_block_offset + i*4 + 2] ^= (xor_key >> 16);
-		shikata_file_code[shikata_offset + shikata_encrypted_block_offset + i*4 + 3] ^= (xor_key >> 24);
-		new_int_value = get_shikata_initial_key(shikata_file_code, shikata_offset + shikata_encrypted_block_offset + i*4 - shikata_initial_key_offset);
+		new_int_value = get_int_value(code->encrypted_block + i*4, 4)^xor_key;
+		set_int_value(code->encrypted_block + i++*4, new_int_value);
 		xor_key += new_int_value;
-		++i;
 	}
 	return(0);
 }
@@ -114,6 +109,7 @@ int main(int argc, char const *argv[])
 		shikata_initial_key,
 		shikata_decryption_len;
 	FILE *hEncoded, *hDecoded;
+	struct shikata_prolog_struct *shikata_prolog;
 
 	if (argc != 3)	{
 		printf("Usage: shikata_decrypt <encrypted_file_name> <decrypted_file_name>\n");
@@ -129,12 +125,13 @@ int main(int argc, char const *argv[])
 
 	shikata_offset = find_shikata(shikata_file_code, encoded_file_size);
 	printf("encoder/x86/shikata_ga_nai found at offset: %x\n", shikata_offset);
+	shikata_prolog = transform_to_prolog(shikata_file_code + shikata_offset);
 
-	shikata_initial_key = get_shikata_initial_key(shikata_file_code, shikata_offset);
+	shikata_initial_key = get_int_value(shikata_prolog->initial_key, 4);
 	printf("shikata initial xor key: %x\n", shikata_initial_key);
-	shikata_decryption_len = get_shikata_decryption_len(shikata_file_code, shikata_offset);
+	shikata_decryption_len = get_int_value(shikata_prolog->decryption_len, 2);
 	printf("shikata decryption length: %x\n", shikata_decryption_len);
-	decrypt_shikata(shikata_file_code, shikata_offset, shikata_initial_key, shikata_decryption_len);
+	decrypt_shikata_new(shikata_prolog);
 	printf("decrypted file: %s\n", argv[2]);
 	fwrite(shikata_file_code, 1, encoded_file_size, hDecoded);
 	
